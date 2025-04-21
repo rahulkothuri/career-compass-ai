ed4660acc9266a647e13a71bec945da49a764b54
Rahul Kothuri <rahulkothuri67@gmail.com>
2025-04-21 19:46:24 +0530

    Revert "Fix: Resolve 'global is not defined' error"
    This reverts commit edc2f500751bb725b53d702847dda99c66748944.

--- src/polyfills.ts
+++
@@ -1,3 +0,0 @@
-
-// Polyfill for missing browser APIs that the Amazon Cognito Identity JS library expects
-window.global = window;
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -19,9 +18,5 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  define: {
-    // This provides a fix for the 'global is not defined' error
-    global: 'globalThis',
   },
 }));
